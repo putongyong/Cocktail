113bce40b6c4e2e444f92013ae8ba852e22c0cdb
putongyong <117098471+putongyong@users.noreply.github.com>
2023-05-30 15:01:35 +0200

    feature:add new style

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import ProductList from "./components/productlist";
+import Intro from "./components/intro";
 
 export default function Root() {
     return (
@@ -12,23 +13,8 @@ export default function Root() {
 				<h2 class="col-12 text-center tm-section-title">Welcome to Cocktail Room</h2>
 				<p class="col-12 text-center">Total 3 HTML pages are included in this template. Header image has a parallax effect. You can feel free to download, edit and use this TemplateMo layout for your commercial or non-commercial websites.</p>
 			</header>
+			<Intro />
 			<ProductList/>
-			<div class="tm-section tm-container-inner">
-				<div class="row">
-					<div class="col-md-6">
-						<figure class="tm-description-figure">
-							<img src="img/img-01.jpg" alt="Image" class="img-fluid" />
-						</figure>
-					</div>
-					<div class="col-md-6">
-						<div class="tm-description-box"> 
-							<h4 class="tm-gallery-title">Maecenas nulla neque</h4>
-							<p class="tm-mb-45">Redistributing this template as a downloadable ZIP file on any template collection site is strictly prohibited. You will need to <a rel="nofollow" href="https://templatemo.com/contact">talk to us</a> for additional permissions about our templates. Thank you.</p>
-							<a href="about.html" class="tm-btn tm-btn-default tm-right">Read More</a>
-						</div>
-					</div>
-				</div>
-			</div>
 		</main>
 		<Footer/>
 	</div>
